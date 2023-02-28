ef957eba24a8e85a5d8bf9f1130e22fc725da034
arafatrimon <rimonarafat2020@gmail.com>
2023-02-28 17:42:47 +0600

    first commit

---
+++ src/test/java/com/example/super_shop_management_system/SuperShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.super_shop_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperShopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
